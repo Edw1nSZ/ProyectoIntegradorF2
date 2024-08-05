cd43c9fb3d8201b5187d713537d22aea69c85c76
Edw1nSZ <edwincruz2basico@gmail.com>
2024-08-04 22:04:47 -0600

    prueba de swagerr implementado(fallida)

--- src/main/java/com/Integrador/proyecto/config/SwaggerConfig.java
+++ src/main/java/com/Integrador/proyecto/config/SwaggerConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
