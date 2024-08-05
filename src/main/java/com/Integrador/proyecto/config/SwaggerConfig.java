50fec71071e885090e94ba63ba0463b5fe7cab13
Edw1nSZ <edwincruz2basico@gmail.com>
2024-08-04 22:32:08 -0600

    vonfig eliminado

--- src/main/java/com/Integrador/proyecto/config/SwaggerConfig.java
+++ src/main/java/com/Integrador/proyecto/config/SwaggerConfig.java
@@ -1,28 +1,5 @@
 package com.Integrador.proyecto.config;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import org.springdoc.core.GroupedOpenApi;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
 public class SwaggerConfig {
 
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("API prueba swagger")
-                        .version("1.0")
-                        .description("Una prueba de asignacion para mi proyecto de Ada"));
-    }
-
-    @Bean
-    public GroupedOpenApi publicApi() {
-        return GroupedOpenApi.builder()
-                .group("public-api")
-                .pathsToMatch("/**")
-                .build();
-    }
 }
